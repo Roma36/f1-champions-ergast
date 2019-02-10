9163028292deb91d20469f742145c804611a98cc
Roman Grinovski <romagrin36@gmail.com>
2019-02-11 02:27:44 +0300

    Fix cors issue at github pages production build

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
 export default {
-  api: 'http://ergast.com/api/',
+  api: `http${process.env.NODE_ENV === 'production' ? 's' : ''}://ergast.com/api/`,
   publicUrl: process.env.PUBLIC_URL,
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import cors from './cors';
+
+if (process.env.NODE_ENV === 'production') {
+  cors();
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
