7b40ad86e24f74963853d124277fe6bf1b37ae6a
Roman Grinovski <romagrin36@gmail.com>
2019-02-11 02:08:39 +0300

    Updates

---
+++ precache-manifest.b00a842d4229d96788ebe783e2370e8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "10b2bf5c07dd4d645431",
+    "url": "/f1-champions-ergast/static/js/runtime~main.10b2bf5c.js"
+  },
+  {
+    "revision": "5defe1ec9df954e51770",
+    "url": "/f1-champions-ergast/static/js/main.5defe1ec.chunk.js"
+  },
+  {
+    "revision": "144762ec173fd6fb328e",
+    "url": "/f1-champions-ergast/static/js/1.144762ec.chunk.js"
+  },
+  {
+    "revision": "5defe1ec9df954e51770",
+    "url": "/f1-champions-ergast/static/css/main.afd7f6f3.chunk.css"
+  },
+  {
+    "revision": "2fbc8632b04bb14d3e9479d77e88a390",
+    "url": "/f1-champions-ergast/index.html"
+  }
+];
